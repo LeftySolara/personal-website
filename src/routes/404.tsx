d2a59eb6ac75acabebdc37799bc35549c63a882d
Julianne Adams <julianne@julianneadams.dev>
2023-11-26 07:23:05 -0500

    Fix: TypeScript errors preventing build

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,17 +1,28 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div id="error-page">
+        <h1>Oops! {error.status}</h1>
+        <p>{error.statusText}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div id="error-page">
+        <h1>Oops! Unexpected error.</h1>
+        <p>Something went wrong.</p>
+        <p>
+          <i>{error.message}</i>
+        </p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default ErrorPage;
--- src/test/testUtils.ts
+++ src/test/testUtils.ts
@@ -1,8 +1,13 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { JSXElementConstructor, ReactElement } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-const renderWithRouter = (ui, { route = "/" } = {}) => {
+const renderWithRouter = (
+  // @eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ui: ReactElement<any, string | JSXElementConstructor<any>>,
+  { route = "/" } = {},
+) => {
   window.history.pushState({}, "Test page", route);
 
   return {
