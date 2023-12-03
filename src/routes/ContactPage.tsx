c0cf15a82f937dc2a835e8724117b64b2167729d
Julianne Adams <julianne@julianneadams.dev>
2023-12-03 09:56:17 -0500

    Add: contact page content

--- src/routes/ContactPage.tsx
+++ src/routes/ContactPage.tsx
@@ -1,8 +1,25 @@
 const ContactPage = () => {
   return (
     <div id="contact-page">
-      <h1>Contact Me</h1>
-      <i>This page is currently under construction.</i>
+      <div id="main-content">
+        <h1>Contact Me</h1>
+        <p>
+          For inquiries about me or my work, feel free to send me an email at{" "}
+          <a href="mailto:julianne@julianneadams.dev">
+            julianne@julianneadams.dev
+          </a>
+          .
+        </p>
+        <p>
+          I can also be reached through{" "}
+          <a href="https://github.com/LeftySolara">GitHub</a>,{" "}
+          <a href="https://linkedin.com/in/julianneadams">LinkedIn,</a> and{" "}
+          <a href="https://twitter.com/leftysolara">Twitter</a>.
+        </p>
+      </div>
+      <div id="contact-image-container">
+        <img src="contact-image.jpg" />
+      </div>
     </div>
   );
 };
