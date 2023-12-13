6163af97f790cf95761528c308d8d76ffe2bfd4a
Julianne Adams <julianne@julianneadams.dev>
2023-12-13 05:16:37 -0500

    Add: personal website project info

--- src/routes/ProjectsPage.tsx
+++ src/routes/ProjectsPage.tsx
@@ -5,7 +5,7 @@ const ProjectsPage = () => {
     <div id="projects-page">
       <div className="projects-header">
         <h1>My Projects</h1>
-        <div className="projects-button-container">
+        <div className="projects-header-button-container">
           <ButtonLink href="/" color="rosewater">
             HOME
           </ButtonLink>
@@ -22,6 +22,45 @@ const ProjectsPage = () => {
           with. To see all of my projects, please visit my{" "}
           <a href="https://github.com/leftysolara">GitHub</a> profile.
         </p>
+      </div>
+      <div className="projects-list">
+        <div className="project rosewater">
+          <div className="project-image-container">
+            <img className="project-image" src="personal-website.png" />
+          </div>
+          <div className="project-info-container">
+            <h2>Personal Website</h2>
+            <p>
+              This is the site you&apos;re looking at now! It&apos;s my personal
+              website which provides information about me, my projects, and how
+              to get in touch.
+            </p>
+            <p>
+              The site was built using TypeScript, Vite, React, and Sass/SCSS.
+              Tests are run using Vitest and React Testing Library.
+            </p>
+            <p>
+              With this project I learned a few best practices for Sass/SCSS, as
+              this was my first <em>real</em> project that used it. I also
+              improved my testing skills a bit. The biggest thing I learned with
+              this project was how to deploy a Vite/React application on AWS.
+            </p>
+            <p>
+              <strong>Technologies used</strong>: Typescript, React, Sass/SCSS
+            </p>
+            <div className="project-button-container">
+              <ButtonLink
+                href="https://github.com/leftysolara/personal-website"
+                color="sky"
+              >
+                VIEW THE CODE
+              </ButtonLink>
+              <ButtonLink href="/" color="green">
+                VISIT THE SITE
+              </ButtonLink>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
